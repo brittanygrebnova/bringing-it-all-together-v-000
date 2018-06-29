03ee307215cfe721a55423c814ddcb01ca606463
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-06-29 02:55:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,8 +73,15 @@ class Dog
     #binding.pry
   end
   
-  def self.find_or_create_by
-    
+  def self.find_or_create_by(name:, album:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
     
   
